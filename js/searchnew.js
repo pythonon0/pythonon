24bdb0db492915e9e934a916b6e09670ba150776
pythonon0 <1770796956@qq.com>
2018-09-17 21:58:20 +0800

    modify

--- js/searchnew.js
+++ js/searchnew.js
@@ -1,11 +1,14 @@
 var input = document.getElementsByClassName("search-input")[0];
 var serachImg = document.getElementsByClassName("search-img")[0];
+var xxx = document.getElementsByClassName('xxx')[0];
+var noSign = document.getElementsByClassName('no-sign')[0];
 document.addEventListener('keypress',function(e){
     if(e.keyCode == 13){
         if(input.value == "汽车工程师") {
             window.open("https://xdxxdxxdxxdxxdx.github.io/pythononplus/searchinner.html","_blank");
         }else{
-            alert("请输入正确内容！");
+            xxx.innerHTML = input.value;
+            noSign.style.display = "block";
         }        
     }
 });
@@ -14,6 +17,11 @@ serachImg.addEventListener('click',function(){
     if(input.value == "汽车工程师") {
         window.open("https://xdxxdxxdxxdxxdx.github.io/pythononplus/searchinner.html","_blank");
     }else{
-        alert("请输入正确内容！");
+        xxx.innerHTML = input.value;
+        noSign.style.display = "block";
     }        
+});
+
+document.getElementsByClassName('confirm')[0].addEventListener('click',function(){
+    noSign.style.display = "none";       
 });
